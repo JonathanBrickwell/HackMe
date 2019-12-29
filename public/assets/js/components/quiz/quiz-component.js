ca0d5323acac8c8d5e4205bfebe3a19b7640e086
JonathanBrickwell <jonathanbrickwell@gmail.com>
2019-12-29 16:29:31 +0100

    #7 - Add: Quiz questions added

--- public/assets/js/components/quiz/quiz-component.js
+++ public/assets/js/components/quiz/quiz-component.js
@@ -1,13 +1,30 @@
+const userInputField = document.getElementById("userInput");
+const questionText = document.getElementById("question");
 
-let userInput = "";
-let userInputField = document.getElementById("userInput");
+const startTheGameWrapper = document.getElementById('start');
+const quizGameWrapper = document.getElementById('quiz');
 
+const startButton = document.getElementById('startBtn');
+startButton.addEventListener("click", startTheGame);
+
+var currentAnswerIndex = 0;
 
 function QuizComponent(quizData) {
     this.quizData = quizData;
     var highScore = 0;
-    var currentAnswerIndex = 0;
+    var userInput = " ";
     var score = 0;
+}
+
+function startTheGame() {
+    console.log("Started!");
+    startTheGameWrapper.classList.add('disabled');
+    quizGameWrapper.classList.remove('disabled');
+    DisplayNextQuestion();
+}
+
+function DisplayNextQuestion() {
+    questionText.innerHTML = questionsModel[currentAnswerIndex].question;
 }
 
 function ResetUserInput() {
@@ -23,12 +40,7 @@ function CheckCorrectAnswer() {
      
 }
 
-function DisplayNextQuestion() {
 
-}
 
-function Init() {
-    console.log(QuizComponent());
-}
 
 
--- public/assets/js/models/quiz-questions-model.js
+++ public/assets/js/models/quiz-questions-model.js
@@ -1,11 +1,11 @@
-var questions = [
+var questionsModel = [
     {
-        "Question #1": "This is the dummy question. \n if(!password) \n { \n pressTheButton(); \n }"
+        question: "This is the dummy question. \n if(!password) \n { \n pressTheButton(); \n }"
     },
     {
-        "Question #2": "This time the password is hidden within this page. \n Hint: I am revealed when inspected."
+        question: "This time the password is hidden within this page. \n Hint: I am revealed when inspected."
     },
     {
-        "Questions #3": "This text is the password. \n Hint: Don't be sha to be the one that's going to find it."
+        question: "This text is the password. \n Hint: Don't be sha to be the one that's going to find it."
     }
 ]
